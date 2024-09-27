a7cffe7c5fdd0c9fb8e1ce6e1557cc4a4c450861
Ionut Alexandru Popescu <pialex2032@gmail.com>
2024-09-27 18:18:41 +0300

    update

--- app/src/header.rs
+++ app/src/header.rs
@@ -6,10 +6,9 @@ stylance::import_style!(style, "./header.module.scss");
 pub fn simple_header(title: String) -> impl IntoView {
     view! {
         <header class=style::simpleheader>
-           // <A href="/">"Home"</A>
+            <A href="/">"Home"</A> 
             <h1>{title}</h1>
             <div></div>
         </header>
     }
 }
-
