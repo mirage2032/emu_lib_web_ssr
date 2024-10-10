6a5f66ae6b37d3e8f76544787c9043554f1f80aa
Ionut Alexandru Popescu <pialex2032@gmail.com>
2024-10-10 18:48:52 +0300

    update

--- app/src/dashboard/mod.rs
+++ app/src/dashboard/mod.rs
@@ -4,14 +4,16 @@ use crate::utils::icons::Icon;
 use leptos::prelude::*;
 use leptos_meta::Title;
 use std::string::ToString;
-use stylance::import_style;
+use stylance::{classes, import_style};
 
 import_style!(style, "./dashboard.module.scss");
 
 #[component]
-fn nav_button_url(icon:String,name:String,url:String) -> impl IntoView{
+fn nav_button_url(icon:String,name:String,url:String,
+                  #[prop(optional)]
+                  extra_class: String) -> impl IntoView{
     view! {
-        <div class="nav-button">
+        <div class=format!("nav-button{}",extra_class)>
             <a href="/">
                 <Icon name=icon />
             </a>
@@ -21,11 +23,22 @@ fn nav_button_url(icon:String,name:String,url:String) -> impl IntoView{
 
 type CallbackList = Vec<Action<(),()>>;
 #[island]
-fn nav_button_onclick(icon:String,name:String,callback_index:usize) -> impl IntoView
+fn nav_button_onclick(icon:String,name:String,callback_index:usize,
+                      extra_class: Option<String>
+) -> impl IntoView
 where {
+    let extra = match extra_class{
+        Some(data) => format!(" {}",data),
+        None => String::default()
+    };
     let callback = expect_context::<CallbackList>();
     view! {
-        <div class="nav-button" on:click=move |_| {callback[callback_index].dispatch(());}>
+        <div
+            class=format!("nav-button{}",extra)
+            on:click=move |_| {
+                callback[callback_index].dispatch(());
+            }
+        >
             <span>
                 <Icon name=icon />
             </span>
@@ -44,25 +57,43 @@ pub fn dashboard() -> impl IntoView {
     provide_context(context);
     view! {
         <Title text="Dashboard" />
-        <div class=style::dashboardcontainer>
-            <nav>
-                <NavButtonUrl
-                    icon="ri-home-fill".to_string()
-                    name="Home".to_string()
-                    url="/".to_string()
-                />
-                <NavButtonOnclick
-                    icon="ri-logout-box-fill".to_string()
-                    name="Home".to_string()
-                    callback_index=0
-                />
-                <NavButtonOnclick
-                    icon="ri-cpu-line".to_string()
-                    name="Home".to_string()
-                    callback_index=0
-                />
-            </nav>
-            <div>POTATO</div>
+        <div class=style::maincontainer>
+            <div class=style::navcontainer>
+        <nav>
+                <div class=classes!(
+        style::navgroup,
+        style::fullheight
+    )>
+                    <NavButtonUrl
+                        icon="ri-home-fill".to_string()
+                        name="Home".to_string()
+                        url="/".to_string()
+                    />
+                    <NavButtonOnclick
+                        icon="ri-logout-box-fill".to_string()
+                        name="Home".to_string()
+                        extra_class=None
+                        callback_index=0
+                    />
+                    <NavButtonOnclick
+                        icon="ri-cpu-line".to_string()
+                        name="Home".to_string()
+                        extra_class=None
+                        callback_index=0
+                    />
+                </div>
+        <div class=style::navgroup
+        style:height="5rem"
+        >
+                    <NavButtonOnclick
+                        icon="ri-logout-box-fill".to_string()
+                        name="Home".to_string()
+                        extra_class=Some(style::nobottompadding.to_string())
+                        callback_index=0
+                    />
+                </div>
+        </nav>
+            </div>
         </div>
     }
 }
